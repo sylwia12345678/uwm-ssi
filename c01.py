316bcdb762b1f3c0e62a8a8ddbc9607ad8d3de48
sylwia12345678 <57856537+sylwia12345678@users.noreply.github.com>
2019-12-01 22:05:17 +0100

    Update c01.py

--- c01.py
+++ c01.py
@@ -1,16 +1,3 @@
-{
-    "version": "0.2.0",
-    "configurations": [
-        {
-            "name": "Python: Current File",
-            "type": "python",
-            "request": "launch",
-            "program": "${file}",
-            "console": "integratedTerminal"
-        }
-    ]
-} 
-
 import math
 import glob
 import os
